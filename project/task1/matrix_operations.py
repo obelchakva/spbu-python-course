bcc937b7122e1c5b6a87e5523ff9ec465d85872d
obelchakva <obelchakva@gmail.com>
2025-10-02 18:28:42 +0300

    The names of the variables have been replaced

--- project/task1/matrix_operations.py
+++ project/task1/matrix_operations.py
@@ -3,14 +3,14 @@ from typing import List
 # Matrix operations
 
 
-def add_matrices(A: List[List[float]], B: List[List[float]]) -> List[List[float]]:
+def add_matrices(a: List[List[float]], b: List[List[float]]) -> List[List[float]]: # The names of the variables have been replaced
     """
-    Add two matrices
+    add two matrices
 
     Parameters:
 
-    A : List[List[float]] (first input matrix)
-    B : List[List[float]] (second input matrix)
+    a : List[List[float]] (first input matrix)
+    b : List[List[float]] (second input matrix)
 
     Returns:
 
@@ -20,24 +20,24 @@ def add_matrices(A: List[List[float]], B: List[List[float]]) -> List[List[float]
 
     ValueError: if the dimensions of the input matrices do not match
     """
-    if len(A) != len(B) or len(A[0]) != len(B[0]):
+    if len(a) != len(b) or len(a[0]) != len(b[0]):
         raise ValueError("Error: matrices of different dimensions.")
 
-    result_m = [[0.0] * len(A[0]) for i in range(len(A))]
-    for i in range(len(A)):
-        for j in range(len(A[0])):
-            result_m[i][j] = A[i][j] + B[i][j]
+    result_m = [[0.0] * len(a[0]) for i in range(len(a))]
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            result_m[i][j] = a[i][j] + b[i][j]
     return result_m
 
 
-def multiply_matrices(A: List[List[float]], B: List[List[float]]) -> List[List[float]]:
+def multiply_matrices(a: List[List[float]], b: List[List[float]]) -> List[List[float]]: # The names of the variables have been replaced
     """
     Multiply two matrices
 
     Parameters:
 
-    A : List[List[float]] (first input matrix)
-    B : List[List[float]] (second input matrix)
+    a : List[List[float]] (first input matrix)
+    b : List[List[float]] (second input matrix)
 
     Returns:
 
@@ -52,37 +52,37 @@ def multiply_matrices(A: List[List[float]], B: List[List[float]]) -> List[List[f
     >>> matrix_multiplication([[1, 2], [3, 4]], [[5, 6], [7, 8]])
     [[19, 22], [43, 50]]
     """
-    if len(A[0]) != len(B):
+    if len(a[0]) != len(b):
         raise ValueError(
             "Error: number of rows of first matrix and number of columns of second matrix are different."
         )
 
-    result_m = [[0.0] * len(B[0]) for i in range(len(A))]
+    result_m = [[0.0] * len(b[0]) for i in range(len(a))]
 
-    for i in range(len(A)):
-        for j in range(len(B[0])):
-            for k in range(len(B)):
-                result_m[i][j] += A[i][k] * B[k][j]
+    for i in range(len(a)):
+        for j in range(len(b[0])):
+            for k in range(len(b)):
+                result_m[i][j] += a[i][k] * b[k][j]
 
     return result_m
 
 
-def transpose_matrix(A: List[List[float]]) -> List[List[float]]:
+def transpose_matrix(a: List[List[float]]) -> List[List[float]]: # The names of the variables have been replaced
     """
     Transpose a matrix
 
     Parameters:
 
-    A : List[List[float]] (input matrix to be transposed)
+    a : List[List[float]] (input matrix to be transposed)
 
     Returns:
 
     List[List[float]]: transposed matrix
     """
 
-    result_m = [[0.0] * len(A) for i in range(len(A[0]))]
+    result_m = [[0.0] * len(a) for i in range(len(a[0]))]
 
-    for i in range(len(A[0])):
-        for j in range(len(A)):
-            result_m[i][j] = A[j][i]
+    for i in range(len(a[0])):
+        for j in range(len(a)):
+            result_m[i][j] = a[j][i]
     return result_m
